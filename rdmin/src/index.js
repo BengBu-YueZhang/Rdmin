669f9e7c3edc59b71bd16eabc99fab75602764d3
BengBu-YueZhang <zhangyue_coder@163.com>
2018-11-11 10:47:19 +0800

    init

--- rdmin/src/index.js
+++ rdmin/src/index.js
@@ -1,8 +1,7 @@
-import dva from 'dva';
-import './index.css';
+import dva from 'dva'
 
 // 1. Initialize
-const app = dva();
+const app = dva()
 
 // 2. Plugins
 // app.use({});
@@ -11,7 +10,7 @@ const app = dva();
 // app.model(require('./models/example').default);
 
 // 4. Router
-app.router(require('./router').default);
+app.router(require('./router').default)
 
 // 5. Start
-app.start('#root');
+app.start('#root')
